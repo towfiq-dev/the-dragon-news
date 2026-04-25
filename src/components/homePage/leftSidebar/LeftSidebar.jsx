9f78d5f2d6ed4481ee46d4564a241e05a42fc58e
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-04-25 18:58:05 +0600

    all fuctionality added

--- src/components/homePage/leftSidebar/LeftSidebar.jsx
+++ src/components/homePage/leftSidebar/LeftSidebar.jsx
@@ -1,5 +1,9 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import Img1 from '../../../assets/asset/Rectangle 3986-1.png'
+import Img2 from '../../../assets/asset/Rectangle 3986.png'
+import Img3 from '../../../assets/asset/Rectangle 3986-2.png'
 const AllCategory = async()=>{
   const res = await fetch('https://openapi.programming-hero.com/api/news/categories')
   const data = await res.json()
@@ -17,11 +21,6 @@ const LeftSidebar = async({ activeId }) => {
             const {category_id, category_name} = category
             const isActive = activeId === category_id
             return(
-          // <ul key={category_id}>
-          // <li className='border-1 font-semibold border-black px-4 py-2 btn mb-3 w-40'>
-          //   <Link className='inline-block' href={`/category/${category_id}`} >{category_name}</Link>
-          // </li>
-          // </ul>
           <Link key={category_id} href={`/category/${category_id}`} className="w-full">
               <button className={`w-full mb-3 text-left px-4 py-2 rounded cursor-pointer font-medium transition-all
                 ${isActive 
@@ -33,6 +32,26 @@ const LeftSidebar = async({ activeId }) => {
           )})
         }
       </div>
+        <div className="space-y-6 flex flex-col items-center width-full">
+          {/* Swimming Card */}
+          <div className="relative group">
+            <Image src={Img1} alt="Swimming" className="w-full h-auto rounded-lg shadow-sm" />
+            <p className="text-center mt-2 font-semibold text-gray-700 uppercase tracking-wide text-sm italic">Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+          </div>
+
+          {/* Class Card */}
+          <div className="relative">
+            <Image src={Img2} alt="Class" className="w-full h-auto rounded-lg shadow-sm" />
+            <p className="text-center mt-2 font-semibold text-gray-700 uppercase tracking-wide text-sm italic">Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+          </div>
+
+          {/* Playground Card */}
+          <div className="relative">
+            <Image src={Img3} alt="Playground" className="w-full h-auto rounded-lg shadow-sm" />
+            <p className="text-center mt-2 font-semibold text-gray-700 uppercase tracking-wide text-sm italic">Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+          </div>
+        </div>
+
     </div>
   );
 };
