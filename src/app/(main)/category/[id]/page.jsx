2c4f5fe803e7b8022169d56f49e4408076b577a5
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-04-26 11:35:14 +0600

    Auto-update on Sun, Apr 26, 2026 11:35:14 AM

--- src/app/(main)/category/[id]/page.jsx
+++ src/app/(main)/category/[id]/page.jsx
@@ -2,14 +2,10 @@ import LeftSidebar from '@/components/homePage/LeftSidebar/LeftSidebar';
 import NewsHome from '@/components/homePage/newsHome/NewsHome';
 import RightSidebar from '@/components/homePage/RightSidebar/RightSidebar';
 import React from 'react';
-// const getAllNews = async(category_id)=>{
-//   const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
-//   const data = await res.json()
-//   return data.data
-// }
+
 const NewsCategoryPage = async({params}) => {
   const {id} = await params
-  //const allNews = await getAllNews("02")
+
   return (
 <div className="grid grid-cols-12 gap-7">
       {/* LeftSidebar */}
@@ -19,18 +15,7 @@ const NewsCategoryPage = async({params}) => {
       {/* News Home */}
       <div className='col-span-7'>
         <NewsHome category_id={id}/>
-      {/* <div className='border border-black'>
-      <h3 className='font-semibold text-[20px]'>Dragon News Home</h3>
-      <div >
-        {
-          allNews.map((news)=>{return(
-            <div key={news._id} className='p-4 border mb-5'>
-              <h1>{news.title}</h1>
-            </div>
-          )})
-        }
-      </div>
-    </div> */}
+      
       </div>
       {/* RightSidebar */}
       <div className='col-span-3'>
